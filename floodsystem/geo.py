6820d770c62534a30dd035f979452fbd61304bc6
FSmith-03 <fes45@cam.ac.uk>
2022-02-01 13:49:30 +0000

    Task 1C (Error with monitoring stations)

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,9 +7,20 @@ geographical data.
 """
 
 from floodsystem.station import MonitoringStation
+from floodsystem.stationdata import build_station_list
 from .utils import sorted_by_key  # noqa
-
+#Beginning of task 1B
 def stations_by_distance(stations, p):
 
     
     return 
+#Beginning of task 1C
+def river_with_station(station):
+        stations=build_station_list()
+        for station in stations:
+            t = station.river
+        else:
+            t="Error no station found"
+        return print(t)
+#Test of task 1C
+print(river_with_station("Bourton Dickler"))
