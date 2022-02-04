fb1537f17faef82d6e52ed685990c79167f1d6cb
Tyler Martin <trm32@cam.ac.uk>
2022-02-04 03:39:59 +0000

    1B done

---
+++ Task1B.py
@@ -0,0 +1,18 @@
+from floodsystem.stationdata import build_station_list
+
+
+def run():
+    """Requirements for Task 1B"""
+    from floodsystem.geo import stations_by_distance
+    # Build list of stations
+    stations = build_station_list()
+    StationsTuple= stations_by_distance(stations, (52.2053, 0.1218))
+    print(StationsTuple[:10])
+    print(StationsTuple[-10:])
+
+
+
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    run()
+#test
