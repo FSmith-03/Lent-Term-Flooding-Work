156270b9666ce2d2e0d461c3359c092fb9d0ada9
FSmith-03 <fes45@cam.ac.uk>
2022-02-07 10:24:15 +0000

    Cleaned up test functions

--- Task1E.py
+++ Task1E.py
@@ -1,11 +1,12 @@
 
 from floodsystem.geo import rivers_by_station_number
 from floodsystem.stationdata import build_station_list
-def run():
+def stationswithin9():
     stations = build_station_list
     return print(rivers_by_station_number(stations, 9))
 print("-----Task 1E Stations Sorted By Rivers-----")
-run()
 
+stationswithin9()
+#Old Test Function
 def test_function():
-    assert run() == [('River Thames', 56), ('River Avon', 32), ('River Great Ouse', 31), ('River Derwent', 26), ('River Aire', 25), ('River Calder', 24), ('River Severn', 22), ('River Stour', 22), ('River Ouse', 19), ('River Colne', 19)]
+    assert stationswithin9() == [('River Thames', 56), ('River Avon', 32), ('River Great Ouse', 31), ('River Derwent', 26), ('River Aire', 25), ('River Calder', 24), ('River Severn', 22), ('River Stour', 22), ('River Ouse', 19), ('River Colne', 19)]
--- Task1F.py
+++ Task1F.py
@@ -1,22 +1,21 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import inconsistent_typical_range_stations
-import pytest
 #Demonstration of the module typical_range_consistent outputting the True or False for each station
-def run():
+def stationswithinconsistensies():
     tup = ()
-    print("------Task 1F-------")
     ConsistentLevel = []
     stations = build_station_list()
     for station in stations:
         t = station.typical_range_consistent()
-        print(t)
     
     
     t = sorted(inconsistent_typical_range_stations(stations))   
     print(t)          
-run()
+stationswithinconsistensies()
+
+#Old Test Function
 def test_function():
-    assert run() == ['Airmyn', 'Blacktoft', 'Braunton', 'Brentford', 'Broomfleet Weighton Lock', 'East Hull Hedon Road', 'Eastbourne Harbour', 'Fleetwood', 'Goole', 'Hedon Thorn Road Bridge', 'Hedon Westlands Drain', 'Hempholme Pumping Station Roam Drain', 'Hull Barrier Victoria Pier', 'Hull High Flaggs, Lincoln Street', 'Littlehampton', 'Medmerry', 'North America', 'Paull', 'Salt End', 'Sandwich Quay', 'Sindlesham Mill', 'Stone Creek', 'Templers Road', 'Tickton Pumping Station', 'Topsham', 'Totnes', 'Truro Harbour', 'Wilfholme Pumping Station']
+    assert stationswithinconsistensies() == ['Airmyn', 'Blacktoft', 'Braunton', 'Brentford', 'Broomfleet Weighton Lock', 'East Hull Hedon Road', 'Eastbourne Harbour', 'Fleetwood', 'Goole', 'Hedon Thorn Road Bridge', 'Hedon Westlands Drain', 'Hempholme Pumping Station Roam Drain', 'Hull Barrier Victoria Pier', 'Hull High Flaggs, Lincoln Street', 'Littlehampton', 'Medmerry', 'North America', 'Paull', 'Salt End', 'Sandwich Quay', 'Sindlesham Mill', 'Stone Creek', 'Templers Road', 'Tickton Pumping Station', 'Topsham', 'Totnes', 'Truro Harbour', 'Wilfholme Pumping Station']
 
 
 
