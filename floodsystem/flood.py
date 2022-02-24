f50e456a0f77fef4a6c3ce129b95bba8450d68b9
Tyler Martin <trm32@cam.ac.uk>
2022-02-24 03:40:13 +0000

    implemented flood submodule with stations_level_overthreshold function

---
+++ floodsystem/flood.py
@@ -0,0 +1,11 @@
+
+from floodsystem.utils import sorted_by_key
+from .station import MonitoringStation
+
+def stations_level_over_threshold(stations, tol):
+    tuplelist = []                                                                                  #empty list to store tuples
+    for station in stations:                                                                        #loops through stations
+        if not station.relative_water_level() == None and station.relative_water_level() > tol:     #confirms there are reading for water level and above tollerence
+            temp = (station.name, station.relative_water_level())                                   #adds to list if above
+            tuplelist.append(temp)
+    return sorted_by_key(tuplelist,1,True)        
